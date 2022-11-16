3853ea5449c26c3fdcfa3a2f53697cae2ff70217
AdrienRem <43466134+AdrienRem@users.noreply.github.com>
2022-11-16 02:03:02 +0100

    Update queries_milestone_2.sql

--- queries_milestone_2.sql
+++ queries_milestone_2.sql
@@ -1,4 +1,7 @@
-Copy (with filter_comment_authors_2cat as (SELECT yc.author
+-- get (video_id, vid_category, author_id, video_title) where commenter commented on both gaming and News & politics video channel
+
+CREATE TABLE comments_gaming_politics AS
+with filter_comment_authors_2cat as (SELECT yc.author
 FROM (select title, video_id, category_e
       from video_metadata where category_e in ('News & Politics', 'Gaming')
 ) as "vm"
@@ -9,13 +12,25 @@ having count(distinct category_e) = 2),
 author_vid_id as (select yc.author, yc.video_id from youtube_comments yc inner join filter_comment_authors_2cat
     on yc.author = filter_comment_authors_2cat.author)
 
-select vm.video_id, vm.category_e, author_vid_id.author, vm.title from video_metadata vm
+select vm.video_id, vm.category_e, author_vid_id.author, vm.title, vm.description from video_metadata vm
     inner join author_vid_id on author_vid_id.video_id = vm.video_id
-    where category_e in ('News & Politics', 'Gaming')) To '/mnt/slow-disk/output/adrien/comments_gaming_politics.csv' With CSV DELIMITER ',' HEADER;
+    where category_e in ('News & Politics', 'Gaming');
+         
 
+--
+CREATE TABLE video_id_in_description AS
+  SELECT vm.video_id as source_video_id,
+       (regexp_matches(vm.description,
+           '(?:(?:https?:)?\/\/)?(?:(?:www|m)\.)?(?:youtube(-nocookie)?\.com|youtu.be)' ||
+           '\/(?:[\w\-]+\?v=|embed\/|v\/)?([\w\-]{11})', 'g'))[2]::varchar(11) as contained_video_id
 
-Copy (select vm.video_id, vm2.video_id as id_in_desc, vm2.channel_id as channel_in_desc
-from video_metadata vm join
-     video_metadata vm2 on
-     vm.description LIKE '%' || vm2.video_id || '%' OR
-         vm.description LIKE '%' || vm2.channel_id || '%') To '/mnt/slow-disk/output/adrien/videos_rel.csv' With CSV DELIMITER ',' HEADER;
+from video_metadata vm;
+
+--
+CREATE TABLE channel_id_in_description AS
+  SELECT vm.video_id as source_video_id,
+       (regexp_matches(vm.description,
+        '(?:(?:https?:)?\/\/)?(?:(?:www|m)\.)?(?:youtube(-nocookie)?\.com|youtu.be)\/(?:channel|c\/)(\w+)',
+           'g'))[2] as contained_channel_id, vm.description
+
+from video_metadata vm;
